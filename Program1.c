98132954cbda83fea49e61321c0ba4616278224c
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-08-29 14:44:40 +0530

    Create Program1.c

---
+++ Program1.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+  float true_value, approximate_value;
+  printf("Enter true value : ");
+  scanf("%f",&true_value);
+  printf("Enter approximate value :");
+  scanf("%f",&approximate_value);
+  float absolute_error, relative_error, percentage_error;
+  absolute_error = abs(true_value-approximate_value);
+  relative_error = absolute_error/true_value;
+  percentage_error = relative_error*100;
+  printf("Absolute Error : %f\n",absolute_error);
+  printf("Relative Error : %f\n",relative_error);
+  printf("Percentage Error : %f\n",percentage_error);
+  return 0;
+}
