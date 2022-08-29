605b043613a38c738bee60e5d0b3b78f2140dee7
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-08-29 14:49:37 +0530

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -8,7 +8,7 @@ int main()
   printf("Enter approximate value :");
   scanf("%f",&approximate_value);
   float absolute_error, relative_error, percentage_error;
-  absolute_error = abs(true_value-approximate_value);
+  absolute_error = fabs(true_value-approximate_value);
   relative_error = absolute_error/true_value;
   percentage_error = relative_error*100;
   printf("Absolute Error : %f\n",absolute_error);
